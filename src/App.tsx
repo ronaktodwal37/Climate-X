13c6c5f2e19c7169a73f4444349e663f4dd9bfab
ronaktodwal37 <ronaktodwal37@gmail.com>
2026-09-14 08:48:26 +0530

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { Layout } from '@/components/layout/Layout';
 // import { DashboardPage } from '@/pages/DashboardPage';
 // import { DigitalTwinPage } from '@/pages/DigitalTwinPage';
 // import { ScenarioSimulatorPage } from '@/pages/ScenarioSimulatorPage';
-// import { ClimateRiskPage } from '@/pages/ClimateRiskPage';
+import { ClimateRiskPage } from '@/pages/ClimateRiskPage';
 import { AIAdvisorPage } from '@/pages/AIAdvisorPage';
 import { DataSourcesPage } from '@/pages/DataSourcesPage';
 import { ContactPage } from '@/pages/ContactPage';
