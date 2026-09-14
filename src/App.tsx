5edc4a53cb028801d4e078eff06d9e516aa2df6b
ronaktodwal37 <ronaktodwal37@gmail.com>
2026-09-14 08:47:32 +0530

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ function App() {
         {/* <Route element={<Layout />}> */}
           {/* <Route path="/" element={<HomePage />} /> */}
           {/* <Route path="/about" element={<AboutPage />} /> */}
-          {/* <Route path="/dashboard" element={<DashboardPage />} /> */}
+          <Route path="/dashboard" element={<DashboardPage />} />
           <Route path="/digital-twin" element={<DigitalTwinPage />} />
           <Route path="/simulator" element={<ScenarioSimulatorPage />} />
           <Route path="/risk" element={<ClimateRiskPage />} />
