05a4b0af2d1fac466a8d7783dfabf10156990921
ronaktodwal37 <ronaktodwal37@gmail.com>
2026-09-14 08:49:00 +0530

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Layout } from '@/components/layout/Layout';
-// import { HomePage } from '@/pages/HomePage';
+import { HomePage } from '@/pages/HomePage';
 import { AboutPage } from '@/pages/AboutPage';
 import { DashboardPage } from '@/pages/DashboardPage';
 import { DigitalTwinPage } from '@/pages/DigitalTwinPage';
